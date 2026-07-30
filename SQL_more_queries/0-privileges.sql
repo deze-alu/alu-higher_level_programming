e39dcd6df7770be041e40250084dbef89a20faf0
Daniel Eze <dan4allu93@gmail.com>
2026-07-30 23:32:37 +0200

    fix(sql-queries): align dynamic privileges for both listed users

--- SQL_more_queries/0-privileges.sql
+++ SQL_more_queries/0-privileges.sql
@@ -3,6 +3,12 @@ REVOKE AUDIT_ABORT_EXEMPT, AUTHENTICATION_POLICY_ADMIN, FIREWALL_EXEMPT,
        GROUP_REPLICATION_STREAM, PASSWORDLESS_USER_ADMIN,
        SENSITIVE_VARIABLES_OBSERVER, TELEMETRY_LOG_ADMIN
     ON *.* FROM 'user_0d_1'@'localhost';
--- Lists all the privileges of the MySQL users user_0d_1 and user_0d_2
+-- Lists all the privileges of the MySQL user user_0d_1
 SHOW GRANTS FOR 'user_0d_1'@'localhost';
+-- Drops the same recent dynamic privileges for the second user
+REVOKE AUDIT_ABORT_EXEMPT, AUTHENTICATION_POLICY_ADMIN, FIREWALL_EXEMPT,
+       GROUP_REPLICATION_STREAM, PASSWORDLESS_USER_ADMIN,
+       SENSITIVE_VARIABLES_OBSERVER, TELEMETRY_LOG_ADMIN
+    ON *.* FROM 'user_0d_2'@'localhost';
+-- Lists all the privileges of the MySQL user user_0d_2
 SHOW GRANTS FOR 'user_0d_2'@'localhost';
